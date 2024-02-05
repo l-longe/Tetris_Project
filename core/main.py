efcd8b2f7193019c3dfa3a14de4fa6265645d849
Lathan <lathanlonge@hotmail.com>
2024-02-05 13:35:13 +0000

    Moves background colour constants to a new module

--- core/main.py
+++ core/main.py
@@ -1,21 +1,12 @@
 import pygame
 from pygame.locals import Rect
 from screen_constants import BOARD_ROW_COUNT, BOARD_COLUMN_COUNT, BLOCK_SIZE, SCREEN_SIZE
+from ui_constants import COLOUR_BKG_GREY_1, COLOUR_BKG_GREY_3
 
 FRAME_RATE = 30
 
 TEXT_TITLE = "TETRIS"
 """ The title of the game. """
-
-# Background colors
-COLOUR_BKG_BLACK = (10, 10, 10)
-COLOUR_BKG_WHITE = (255, 255, 255)
-COLOUR_BKG_GREY_1 = (26, 26, 26)
-COLOUR_BKG_GREY_2 = (35, 35, 35)
-COLOUR_BKG_GREY_3 = (55, 55, 55)
-COLOUR_BKG_BEIGE = (255, 255, 204)
-COLOUR_BKG_BROWN = (153, 102, 51)
-
 
 def draw_grid():
     for x in range(BOARD_COLUMN_COUNT):
---
+++ core/ui_constants.py
@@ -0,0 +1,11 @@
+# ui_constants.py
+""" This module contains hard-coded settings for the UI. """
+
+# Background colors
+COLOUR_BKG_BLACK = (10, 10, 10)
+COLOUR_BKG_WHITE = (255, 255, 255)
+COLOUR_BKG_GREY_1 = (26, 26, 26)
+COLOUR_BKG_GREY_2 = (35, 35, 35)
+COLOUR_BKG_GREY_3 = (55, 55, 55)
+COLOUR_BKG_BEIGE = (255, 255, 204)
+COLOUR_BKG_BROWN = (153, 102, 51)
