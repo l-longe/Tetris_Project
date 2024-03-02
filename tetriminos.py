5288f67f9b13221b2f1f7bcc4085b3d6ae36b176
Lathan <lathanlonge@hotmail.com>
2024-03-02 21:18:08 +0000

    Adds tetriminos file

---
+++ tetriminos.py
@@ -0,0 +1,5 @@
+# tetriminos.py
+""" This module contains the tetrimino class and the 7 standard tetrimino-shapes. """
+
+_ls_tetriminos = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
+""" The list of all tetriminos. """
